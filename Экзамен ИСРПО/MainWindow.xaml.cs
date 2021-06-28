b9382323f48a50d2b87ba168db3e2b9d4b940060
gr691_aas <gr691_aas@L308-6>
2021-06-28 11:18:32 +0700

    Добавление кода в MainWindow.xaml.cs

--- Экзамен ИСРПО/MainWindow.xaml.cs
+++ Экзамен ИСРПО/MainWindow.xaml.cs
@@ -18,9 +18,24 @@ namespace Экзамен_ИСРПО
 
     public partial class MainWindow : Window
     {
+        Database1Entities db;
         public MainWindow()
         {
             InitializeComponent();
+            db = new Database1Entities();
         }
+
+        private void Auth_Enter(object sender, RoutedEventArgs e)
+        {
+
+            Auth auth = new Auth();
+            var authorization = db.User.FirstOrDefault(ch => ch.Login == Login.Text && ch.Password == Password.Password);
+            if (auth.Enter(Login.Text, Password.Password) == true)
+            {
+                MessageBox.Show("Вход выполнен", "Авторизация", MessageBoxButton.OK, MessageBoxImage.Information);
+                Application.Current.Shutdown();
+            }
+        }
+
     }
 }
