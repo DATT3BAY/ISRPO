2f9548c92bda9c0ccf3928a79ab59bd2fcdbaa78
gr691_aas <gr691_aas@L308-6>
2021-06-28 11:05:50 +0700

    Добавление локальной БД,  таблицы и модели

--- Экзамен ИСРПО/Auth.cs
+++ Экзамен ИСРПО/Auth.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,21 @@ namespace Экзамен_ИСРПО
 {
     public class Auth
     {
-
+        public bool Enter(string login, string password)
+        {
+            Database1Entities db = new Database1Entities();
+            if (login == "" || password == "")
+            {
+                MessageBox.Show("Вы не заполнили все поля", "Авторизация", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            var auth_check = db.User.FirstOrDefault(ch => ch.Login == login && ch.Password == password);
+            if (auth_check == null)
+            {
+                MessageBox.Show("Логин или пароль введены не верно", "Авторизация", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
     }
 }
